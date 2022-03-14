1708c439cbd6194a38913d54a003c0b46dfb6bcd
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-03-14 12:36:26 +0100

    basic list done

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,11 +6,12 @@ const MovieList = ({movies}) => {
     return(
 
         <div className="movies__row row">
+
             {movies && movies.map((movie, idx) => (
                 <div key={idx} className="movies__movie">
 
-                    {/*<img className="movies__poster" src={movie.Poster} alt="movie poster"/>*/}
-                    <img className="movies__poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="movie poster"/>
+                    <img className="movies__poster" src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`: "https://i.ibb.co/gy133B0/poster.jpg"} alt="movie poster"/>
+
                 </div>
             ))}
         </div>
